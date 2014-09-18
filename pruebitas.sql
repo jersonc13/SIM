463668581da86548d44f1183618c648bd82442ef
Jose Vinces <jvinceso@gmail.com>
2014-09-18 00:53:58 -0500

    sql

--- pruebitas.sql
+++ pruebitas.sql
@@ -1,4 +1,3 @@
 -- select * from sim_persona p inner join sim_usuario u where u.nidpersona = p.nidpersona
 -- select ou.*,o.copcion from sim_opcionusuario ou  inner join sim_opcion o on o.nidopcion = ou.nidopcion  where ou.nidusuario = 18
--- select * from sim_opcionusuario where nidusuario  = 18  -- and nestado = 1
-select * from sim_usuario
+ select * from sim_opcionusuario where nidusuario  = 18  -- and nestado = 1
